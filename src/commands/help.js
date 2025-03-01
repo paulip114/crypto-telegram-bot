05a83ae2414d733655b26b50eb202986083c470f
paulip114 <paulip114@gmail.com>
2025-03-01 18:16:19 +0800

    update command instruction

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,6 +5,7 @@ const commands = `
 /news \\- Recent Crypto news and GPT advice \\)  
 /buy \\- Buy crypto \\(e\\.g\\., /buy BTCUSDT 0\\.01\\)  
 /sell \\- Sell crypto \\(e\\.g\\., /sell BTCUSDT 0\\.01\\)
+/setting \\- Set your Binance API keys \\(e\\.g\\., /setting YOUR_API_KEY YOUR_API_SECRET\\)
 /dex \\- Get Dex wallet balance \\(e\\.g\\., /dex \\<address\\>\\)
 `;
 
--- src/commands/setBinance.js
+++ src/commands/setBinance.js
@@ -3,7 +3,7 @@ const { storeUserBinanceKey } = require("../CEX/binanceStorage");
 module.exports = async (ctx) => {
   const messageParts = ctx.message.text.split(" ");
   if (messageParts.length !== 3) {
-    return ctx.reply("❌ Usage: /setbinance YOUR_API_KEY YOUR_API_SECRET");
+    return ctx.reply("❌ Usage: /setting YOUR_API_KEY YOUR_API_SECRET");
   }
 
   const userId = ctx.message.from.id;
